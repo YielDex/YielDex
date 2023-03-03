aecebb1363fa349d0a85a9a986ad2e3d516fbfb0
Nandy Bâ <nandyba@yahoo.fr>
2023-03-03 13:54:16 -0700

    Add ability to place wBTC order

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Web3Modal } from '@web3modal/react';
+
+
+// import { Web3Modal } from '@web3modal/react';
 import { providers } from '@web3modal/ethereum';
 import { gnosisTestnet, cronosTestnet, mumbaiTestnet } from "./utils/network";
 import { useState, useEffect } from 'react';
 
-import { ethers } from "ethers";
-import { useSigner, useAccount, Web3Button } from '@web3modal/react'
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon, goerli } from "wagmi/chains";
+import { useSigner, useAccount } from 'wagmi'
+
+import { Web3Button } from "@web3modal/react";
 
 import { useWeb3Polygon } from './hooks/useWeb3';
 
@@ -13,36 +24,78 @@ import FormHead from './componants/FormHead';
 
 import Strategies from './componants/Strategies';
 
-const config = {
-  projectId: '1bd4139fcba9da0ebb55e2d5ffa1d12d',
-  theme: 'dark',
-  accentColor: 'default',
-  ethereum: {
-    appName: 'MOMENTO',
-    autoConnect: true,
-    chains: [
-      // gnosisTestnet,
-      // cronosTestnet,
-      mumbaiTestnet,
-    ],
-    providers: [
-      providers.walletConnectProvider({
-        projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d",
-      }),
-    ],
-  }
-};
+// const config = {
+//   projectId: '1bd4139fcba9da0ebb55e2d5ffa1d12d',
+//   theme: 'dark',
+//   accentColor: 'default',
+//   ethereum: {
+//     appName: 'MOMENTO',
+//     autoConnect: true,
+//     chains: [
+//       goerli
+//       // gnosisTestnet,
+//       // cronosTestnet,
+//       // mumbaiTestnet,
+
+//     ],
+//     providers: [
+//       providers.walletConnectProvider({
+//         projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d",
+//       }),
+//     ],
+//   }
+// };
+
+const chains = [goerli]
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({
+    projectId: '1bd4139fcba9da0ebb55e2d5ffa1d12d',
+    version: "2", // "1" or "2"
+    appName: "web3Modal",
+    chains,
+  }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 const App = () => {
+
+  return(
+    <>
+      <Web3Modal
+        projectId='1bd4139fcba9da0ebb55e2d5ffa1d12d'
+        ethereumClient={ethereumClient}
+      />
+      <WagmiConfig client={wagmiClient}>
+        <Home />
+      </WagmiConfig>
+    </>
+    
+  );
+}
+
+const Home = () => {
 
   const [page, setPage] = useState('strategies');
   const [selectedBuyAsset, setSelectedBuyAsset] = useState("");
   const [selectedUnderlayingAsset, setSelectedUnderlayingAsset] = useState("");
 
-  const { account, isReady } = useAccount();
+  const { address, isConnected } = useAccount();
   const web3Polygon = useWeb3Polygon();
 
-  console.log(account.address)
+  useEffect(() => {
+    if (isConnected) {
+      console.log('account', address);
+    }
+  }, [isConnected, address])
 
   const shortenString = (stringToShorten) => {
       const firstPart = stringToShorten.substring(0, 6);
@@ -50,26 +103,37 @@ const App = () => {
       return `${firstPart}..${secondPart}`;
   }
 
-  return(
-    <>
-      <Web3Modal config={config}/>
-      <div className="main-content">
-        <h3 className="text-white main-title">MOMENTO</h3>
-        <div className="flex-row">
-            <p className="text-white">{shortenString(account.address)}</p> <Web3Button/><br />
-        </div>
-        <FormHead
-          underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
-          buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
-        />
-        {page === 'strategies' && <Strategies
-          underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
-          buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
-        />}
-      </div>
-    </>
+  // useEffect(() => {
     
-  );
+
+
+  //   (async () => {
+  //     
+  //   })()
+      
+
+  // })
+
+  return (
+    <div className="main-content">
+      <h3 className="text-white main-title">MOMENTO</h3>
+      <div className="flex-row">
+          { isConnected && <p className="text-white">{shortenString(address)}</p> }
+          <Web3Button/><br />
+      </div>
+      <FormHead
+        underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
+        buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
+      />
+      {page === 'strategies' && <Strategies
+        underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
+        buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
+      />}
+    </div>
+  )
+
 }
+
+  
 
 export default App;
--- src/componants/Block.js
+++ src/componants/Block.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './PriceOracle'
+import { useAccount } from 'wagmi'
+import { prepareWriteContract, writeContract } from '@wagmi/core'
 
 const Block = ({ strategyName, image, score, cryptoLogo, buyAssetState, underlayingAssetState }) => {
   const {selectedBuyAsset, setSelectedBuyAsset} = buyAssetState;
@@ -8,6 +10,7 @@ const Block = ({ strategyName, image, score, cryptoLogo, buyAssetState, underlay
   const [isModalOpen, setIsModalOpen] = useState(false);
   
   const [orderPrice, setOrderPrice] = useState("");
+  const { address, isConnected } = useAccount();
 
   const handleAssetChange = (event) => {
     setSelectedBuyAsset(event.target.value);
@@ -24,6 +27,28 @@ const Block = ({ strategyName, image, score, cryptoLogo, buyAssetState, underlay
   const handleModalOpen = () => {
     setIsModalOpen(true);
   };
+
+  const placeOrder = () => {
+    if(isConnected) {
+      (async () => {
+        const DAI_GOERLI_Address = '0xBa8DCeD3512925e52FE67b1b5329187589072A55'
+        const wBTC_GOERLI_Address = '0x45AC379F019E48ca5dAC02E54F406F99F5088099'
+        const config = await prepareWriteContract({
+          address: '0xb78806a3f7F0A45Ef8179841d13a304abf07f105',
+          abi: [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"","type":"string"},{"indexed":false,"internalType":"address","name":"","type":"address"}],"name":"construct","type":"event"},{"inputs":[],"name":"admin","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_orderNonce","type":"uint256"}],"name":"cancelOrder","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"address","name":"fromToken","type":"address"},{"internalType":"address","name":"toToken","type":"address"}],"name":"createOrder","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"dedicatedMsgSender","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"fundsOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getExecutorAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"orderNonce","type":"uint256"}],"name":"getOrder","outputs":[{"components":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"address","name":"fromToken","type":"address"},{"internalType":"address","name":"toToken","type":"address"},{"internalType":"bytes32","name":"orderId","type":"bytes32"},{"internalType":"bool","name":"isExecuted","type":"bool"}],"internalType":"struct OrderDatas","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ops","outputs":[{"internalType":"contract IOps","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"orderExecutor","outputs":[{"internalType":"contract OrderExecutor","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"orders","outputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"price","type":"uint256"},{"internalType":"address","name":"fromToken","type":"address"},{"internalType":"address","name":"toToken","type":"address"},{"internalType":"bytes32","name":"orderId","type":"bytes32"},{"internalType":"bool","name":"isExecuted","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"orderNonce","type":"uint256"}],"name":"setExecuted","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"price","type":"uint256"}],"name":"setPrice","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"taskTreasury","outputs":[{"internalType":"contract ITaskTreasuryUpgradable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"},{"internalType":"address","name":"_token","type":"address"}],"name":"withdrawFunds","outputs":[],"stateMutability":"nonpayable","type":"function"}],
+          functionName: 'createOrder',
+          args: [orderPrice, wBTC_GOERLI_Address, DAI_GOERLI_Address],
+          chainId: 5,
+        })
+        const { hash } = await writeContract(config)
+        console.log(hash)
+      })()
+    }
+    else{
+      alert("Please connect your wallet")
+    }
+  }
+
 
   const currentOraclePrice = ''
 
@@ -62,7 +87,7 @@ const Block = ({ strategyName, image, score, cryptoLogo, buyAssetState, underlay
                   value={orderPrice}
                   onChange={handlePriceChange}
                 />
-                <button className="modal-btn" onClick={handleModalClose}>
+                <button className="modal-btn" onClick={placeOrder}>
                   Valider
                 </button>
             </div>
