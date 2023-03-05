5b23970692147eb5cc584e15c7023c522ed820eb
Nandy Bâ <nandyba@yahoo.fr>
2023-03-04 21:49:36 -0700

    Add Intu special Wallet Connect button

--- src/App.js
+++ src/App.js
@@ -1,154 +1,108 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useState } from "react";
-import { SignClient } from "@walletconnect/sign-client";
-import { Web3Modal } from "@web3modal/standalone";
-import './App.css';
+
+// import { Web3Modal } from '@web3modal/react';
+import { providers } from '@web3modal/ethereum';
+import { gnosisTestnet, cronosTestnet, mumbaiTestnet } from "./utils/network";
+import { useState, useEffect } from 'react';
+
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { arbitrum, mainnet, polygon, goerli } from "wagmi/chains";
+import { useSigner, useAccount } from 'wagmi'
+
+import { Web3Button } from "@web3modal/react";
+
+import { useWeb3Polygon } from './hooks/useWeb3';
 
 import FormHead from './componants/FormHead';
+
 import Strategies from './componants/Strategies';
 
-const web3Modal = new Web3Modal({
-  projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d",
-  standaloneChains: ["eip155:5"]
+import IntuButton from './Intu/IntuButton';
+
+// const config = {
+//   projectId: '1bd4139fcba9da0ebb55e2d5ffa1d12d',
+//   theme: 'dark',
+//   accentColor: 'default',
+//   ethereum: {
+//     appName: 'MOMENTO',
+//     autoConnect: true,
+//     chains: [
+//       goerli
+//       // gnosisTestnet,
+//       // cronosTestnet,
+//       // mumbaiTestnet,
+
+//     ],
+//     providers: [
+//       providers.walletConnectProvider({
+//         projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d",
+//       }),
+//     ],
+//   }
+// };
+
+const chains = [goerli]
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({
+    projectId: '1bd4139fcba9da0ebb55e2d5ffa1d12d',
+    version: "2", // "1" or "2"
+    appName: "web3Modal",
+    chains,
+  }),
+  provider,
 });
 
-function WalletAbstraction() {
-  const [signClient, setSignClient] = useState();
-  const [sessions, setSessions] = useState([]);
-  const [accounts, setAccounts] = useState([]);
-  const [txHash, setTxHash] = useState();
-  const reset = () => {
-    setAccounts([]);
-    setSessions([]);
-  };
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
-  async function createClient() {
-    try {
-      const client = await SignClient.init({
-        projectId:"1bd4139fcba9da0ebb55e2d5ffa1d12d",
-      })
-      console.log(client);
-      setSignClient(client);
-      await subscribeToEvents(client);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+const App = () => {
 
-  async function handleConnect() {
-    if (!signClient) throw Error("cannot connect, signClient is not defined")
-    try {
-      const proposalNamespace = {
-        eip155: {
-          chains: ["eip155:5"],
-          methods: ["eth_sendTransaction"],
-          events: ["connect", "disconnect"]
-        }
-      };
-
-      const { uri, approval } = await signClient.connect({
-        requiredNamespaces: proposalNamespace
-      });
-
-      console.log('uri', uri);
-
-      if (uri) {
-        web3Modal.openModal({ uri })
-        const sessionNamespace = await approval();
-        console.log("sessionNamespace", sessionNamespace)
-        onSessionConnect(sessionNamespace)
-        //close the modal
-        web3Modal.closeModal();
-      }
-
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async function onSessionConnect(session) {
-    if (!session) throw Error("session doesn't even exist")
-    try {
-      setSessions(session)
-      setAccounts(session.namespaces.eip155.accounts[0].slice(9));
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async function handleDisconnect() {
-    try {
-      await signClient.disconnect({
-        topic: sessions.topic,
-        code: 6000,
-        message: "User disconnected"
-      });
-      reset();
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async function subscribeToEvents(client) {
-    if (!client)
-      throw Error("No events to subscribe to b/c client is not defined");
-
-    try {
-      client.on("session_delete", () => {
-        console.log("user disconnected the session from their wallet");
-        reset();
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  useEffect(() => {
-    if (!signClient) {
-      createClient();
-    }
-  }, [signClient]);
-
-  return (
-    <div className="App">
-      {accounts.length ? (
-        <>
-          <p>{accounts}</p>
-          <button onClick={handleDisconnect}>Disconnect</button>
-          {txHash &&
-            <h2> Check your Transaction Hash {" "}
-              <a
-                href={`https://goerli.etherscan.io/tx/${txHash}`}
-                target="_blank"
-                rel="noreferer noreferrer"
-              >here
-              </a>
-              !
-            </h2>
-          }
-        </>
-      ) : (
-        <button onClick={handleConnect} disabled={!signClient}>
-          Connect
-        </button>
-      )
-      }
-      <App accounts={accounts}/>
-    </div>
+  return(
+    <>
+      <Web3Modal
+        projectId='1bd4139fcba9da0ebb55e2d5ffa1d12d'
+        ethereumClient={ethereumClient}
+      />
+      <WagmiConfig client={wagmiClient}>
+        <Home />
+      </WagmiConfig>
+    </>
+    
   );
 }
 
-const App = ({ accounts}) => {
+const Home = () => {
 
   const [page, setPage] = useState('strategies');
   const [selectedBuyAsset, setSelectedBuyAsset] = useState("");
   const [selectedUnderlayingAsset, setSelectedUnderlayingAsset] = useState("");
 
-  // const { account, isReady } = useAccount();
-  // const web3Polygon = useWeb3Polygon();
+  const { address, isConnected } = useAccount();
+  const web3Polygon = useWeb3Polygon();
 
-  // console.log(account.address)
+
+  // const { signer, isError, isLoading } = useSigner()
+
+  
+
+  useEffect(() => {
+    if (isConnected) {
+      console.log('account', address);
+    }
+  }, [isConnected, address])
 
   const shortenString = (stringToShorten) => {
       const firstPart = stringToShorten.substring(0, 6);
@@ -156,28 +110,38 @@ const App = ({ accounts}) => {
       return `${firstPart}..${secondPart}`;
   }
 
-  return(
-    <>
-      <div className="main-content">
-        <h3 className="text-white main-title">Yieldex</h3>
-        <div className="flex-row">
-            <p className="text-white">{accounts}</p>
-        </div>
-        <FormHead
-          underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
-          buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
-        />
-        {/* {page === 'strategies' && <Strategies
-          underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
-          buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
-        />} */}
-      </div>
-    </>
+  // useEffect(() => {
     
-  );
+
+
+  //   (async () => {
+  //     
+  //   })()
+      
+
+  // })
+
+  return (
+    <div className="main-content">
+      <h3 className="text-white main-title">MOMENTO</h3>
+      <div className="flex-row">
+          { isConnected && <p className="text-white">{shortenString(address)}</p> }
+          <Web3Button/><br />
+          <IntuButton/>
+      </div>
+      <FormHead
+        underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
+        buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
+      />
+      {page === 'strategies' && <Strategies
+        underlayingAssetState={{selectedUnderlayingAsset, setSelectedUnderlayingAsset}}
+        buyAssetState={{selectedBuyAsset, setSelectedBuyAsset}}
+      />}
+    </div>
+  )
+
 }
 
+  
 
-
-
-export default WalletAbstraction;
+export default App;
--- src/Intu/IntuButton/index.js
+++ src/Intu/IntuButton/index.js
@@ -1,10 +1,138 @@
 import './style.css'
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import { SignClient } from "@walletconnect/sign-client";
+import { Web3Modal as Web3ModalStandolone } from "@web3modal/standalone";
 
 import { ethers } from 'ethers';
 import { IntuABI } from '../ABI';
 
+const web3Modal = new Web3ModalStandolone({
+    projectId: "1bd4139fcba9da0ebb55e2d5ffa1d12d",
+    standaloneChains: ["eip155:5"]
+});
 
+function WalletAbstraction() {
+    const [signClient, setSignClient] = useState();
+    const [sessions, setSessions] = useState([]);
+    const [accounts, setAccounts] = useState([]);
+    const [txHash, setTxHash] = useState();
+    const reset = () => {
+      setAccounts([]);
+      setSessions([]);
+    };
+  
+    async function createClient() {
+      try {
+        const client = await SignClient.init({
+          projectId:"1bd4139fcba9da0ebb55e2d5ffa1d12d",
+        })
+        console.log(client);
+        setSignClient(client);
+        await subscribeToEvents(client);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  
+    async function handleConnect() {
+      if (!signClient) throw Error("cannot connect, signClient is not defined")
+      try {
+        const proposalNamespace = {
+          eip155: {
+            chains: ["eip155:5"],
+            methods: ["eth_sendTransaction"],
+            events: ["connect", "disconnect"]
+          }
+        };
+  
+        const { uri, approval } = await signClient.connect({
+          requiredNamespaces: proposalNamespace
+        });
+  
+        console.log('uri', uri);
+  
+        if (uri) {
+          web3Modal.openModal({ uri })
+          const sessionNamespace = await approval();
+          console.log("sessionNamespace", sessionNamespace)
+          onSessionConnect(sessionNamespace)
+          //close the modal
+          web3Modal.closeModal();
+        }
+  
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  
+    async function onSessionConnect(session) {
+      if (!session) throw Error("session doesn't even exist")
+      try {
+        setSessions(session)
+        setAccounts(session.namespaces.eip155.accounts[0].slice(9));
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  
+    async function handleDisconnect() {
+      try {
+        await signClient.disconnect({
+          topic: sessions.topic,
+          code: 6000,
+          message: "User disconnected"
+        });
+        reset();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  
+    async function subscribeToEvents(client) {
+      if (!client)
+        throw Error("No events to subscribe to b/c client is not defined");
+  
+      try {
+        client.on("session_delete", () => {
+          console.log("user disconnected the session from their wallet");
+          reset();
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  
+    useEffect(() => {
+      if (!signClient) {
+        createClient();
+      }
+    }, [signClient]);
+  
+    return (
+      <div className="App">
+        {accounts.length ? (
+          <>
+            <p>{accounts}</p>
+            <button onClick={handleDisconnect}>Disconnect</button>
+            {txHash &&
+              <h2> Check your Transaction Hash {" "}
+                <a
+                  href={`https://goerli.etherscan.io/tx/${txHash}`}
+                  target="_blank"
+                  rel="noreferer noreferrer"
+                >here
+                </a>
+                !
+              </h2>
+            }
+          </>
+        ) : (
+          <button className="btn btn-primary btn-intu" onClick={handleConnect}>Connect with Intu</button>
+        )
+        }
+      </div>
+    );
+  }
 
 const IntuButton = () => {
 
@@ -42,4 +170,4 @@ const IntuButton = () => {
     )
 }
 
-export default IntuButton
+export default WalletAbstraction
