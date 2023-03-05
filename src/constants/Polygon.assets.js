6c13d1b7605da06cdcbf44a8bf9ae6dd17cb697b
Nandy Bâ <nandyba@yahoo.fr>
2023-03-05 05:03:04 -0700

    Add Polygon.assets.js

---
+++ src/constants/Polygon.assets.js
@@ -0,0 +1,47 @@
+const Assets = [
+    {
+        'assetName': 'ETH',
+        'assetLogo': 'https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880',
+        'assetAddress': '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619'
+    },
+    {
+        'assetName': 'wBTC',
+        'assetLogo': 'https://assets.coingecko.com/coins/images/7598/small/wrapped_bitcoin_wbtc.png?1548822744',
+        'assetAddress': '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6'
+    },
+    {
+        'assetName': 'LINK',
+        'assetLogo': 'https://assets.coingecko.com/coins/images/877/small/chainlink-new-logo.png?1547034700',
+        'assetAddress': '0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39'
+    },
+    {
+        'assetName': 'USDC',
+        'assetLogo': 'https://assets.coingecko.com/coins/images/6319/small/USD_Coin_icon.png?1547042389',
+        'assetAddress': '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+    },
+    {
+        'assetName': 'DAI',
+        'assetLogo': 'https://assets.coingecko.com/coins/images/9956/small/4943.png?1636636734',
+        'assetAddress': '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063'
+    }
+]
+
+const Protocols = [
+    {
+        protocolName:"Aave",
+        protocolLogo:'https://cryptologos.cc/logos/aave-aave-logo.png'
+    },
+    {
+        protocolName:"Yearn",
+        protocolLogo:'https://pbs.twimg.com/profile_images/1537201399481700352/nub3IJbS_400x400.jpg'
+    },
+];
+
+const Contracts = [
+    {
+        'contractName': 'OrderBook',
+        'contractAddress': '0x4cfa7d50806a7B2BF0aA7C7a3af421Fb10400449'
+    }
+]
+
+export { Assets, Protocols, Contracts }
